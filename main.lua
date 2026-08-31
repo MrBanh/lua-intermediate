2da17ff44b2d877ddcb64698e17d3d8669b03901
MrBanh <25971310+MrBanh@users.noreply.github.com>
2026-08-31 05:54:45 -0700

    done

--- main.lua
+++ main.lua
@@ -1,14 +1,5 @@
-local function evens(maxN)
-	local n = 0
-	return function()
-		n = n + 2
-		if n <= maxN then
-			return n
-		end
-	end
-end
-
-local n = tonumber(io.read())
-for v in evens(n) do
-	print(v)
+local i = 1
+for line in io.lines() do
+	print(i .. ": " .. line)
+	i = i + 1
 end
