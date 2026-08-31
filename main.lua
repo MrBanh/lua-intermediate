1ac417470f214b5a18a6927e770282b71ca8496a
MrBanh <25971310+MrBanh@users.noreply.github.com>
2026-08-31 05:45:34 -0700

    oops, thought it was coroutine iterator but it's closure iterator

--- main.lua
+++ main.lua
@@ -1,11 +1,11 @@
 local function evens(maxN)
-	local n = 2
-	return coroutine.wrap(function()
-		while n <= maxN do
-			coroutine.yield(n)
-			n = n + 2
+	local n = 0
+	return function()
+		n = n + 2
+		if n <= maxN then
+			return n
 		end
-	end)
+	end
 end
 
 local n = tonumber(io.read())
