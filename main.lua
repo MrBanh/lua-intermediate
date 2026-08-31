4edea1256cec90d08480d376afe9421c4c330cb4
MrBanh <25971310+MrBanh@users.noreply.github.com>
2026-08-30 19:22:31 -0700

    done

--- main.lua
+++ main.lua
@@ -1,14 +1,30 @@
+---@class (exact) Point
+---@field x number
+---@field y number
+
 local mt = {
-    -- __add = function(a, b) ... end,
-    -- __tostring = function(v) ... end,
+	---@param a Point
+	---@param b Point
+	---@return Point
+	__add = function(a, b)
+		return setmetatable({
+			x = a.x + b.x,
+			y = a.y + b.y,
+		}, getmetatable(a))
+	end,
+
+	---@param v Point
+	__tostring = function(v)
+		return string.format("(%d,%d)", v.x, v.y)
+	end,
 }
 
 local function readvec()
-    local line = io.read()
-    local x, y = line:match("(-?%d+) (-?%d+)")
-    return setmetatable({x = tonumber(x), y = tonumber(y)}, mt)
+	local line = io.read()
+	local x, y = line:match("(-?%d+) (-?%d+)")
+	return setmetatable({ x = tonumber(x), y = tonumber(y) }, mt)
 end
 
 local v1 = readvec()
 local v2 = readvec()
--- print(tostring(v1 + v2))
+print(v1 + v2)
