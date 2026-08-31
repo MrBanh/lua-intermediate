296fcfe334e790c90bdb27d643fe788a49f3a1e0
MrBanh <25971310+MrBanh@users.noreply.github.com>
2026-08-31 03:30:36 -0700

    done

--- main.lua
+++ main.lua
@@ -1,32 +1,15 @@
-local Account = {}
-Account.__index = Account
-
-function Account.new(initial)
-	return setmetatable({ bal = initial }, Account)
+local function squares()
+	return coroutine.wrap(function()
+		local i = 1
+		while true do
+			coroutine.yield(i * i)
+			i = i + 1
+		end
+	end)
 end
 
-function Account:deposit(amount)
-	self.bal = self.bal + amount
+local n = tonumber(io.read())
+local co = squares()
+for _ = 1, n do
+	print(co())
 end
-
-function Account:withdraw(amount)
-	self.bal = self.bal + amount
-end
-
-function Account:balance()
-	return self.bal
-end
-
-local initial = tonumber(io.read())
-local acc = Account.new(initial)
-
-for _ = 1, 3 do
-	local amount = tonumber(io.read())
-	if amount >= 0 then
-		acc:deposit(amount)
-	else
-		acc:withdraw(amount)
-	end
-end
-
-print(acc:balance())
